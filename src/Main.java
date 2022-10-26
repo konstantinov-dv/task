d05942ec154aa49d537c9dedf84f66cd33c05b6a
konstantinov-dv <konstantinov.dima.v@gmail.com>
2022-10-26 15:32:11 +0300

    // переменные часть 1

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        // переменные часть 1
+
+        // задача 1
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+
+        // задача 2
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        // задача 3
+        var y = 3.5;
+        System.out.println(dog - y);
+        var x = 1.6;
+        System.out.println(cat - x);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        // задание 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        // задание 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        var q = 3.5;
+        System.out.println(frog / q);
+        System.out.println(frog / q + 4);
+
+
+
+
+
+
+
+
+    }
+}
+
